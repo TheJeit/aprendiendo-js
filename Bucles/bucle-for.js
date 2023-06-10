d027ce4d0510fc436cde6fd8a414c96c4a1fb3c8
TheJeit <soyjeit1@gmail.com>
2023-06-10 17:50:40 -0400

    Ejercicio 2 - For

--- Bucles/bucle-for.js
+++ Bucles/bucle-for.js
@@ -13,19 +13,39 @@
     // Usando un bucle for escribir un script que pida un valor entero y muestre en pantalla una lista
     // de números desde el 0 al valor tecleado. Los números se separarán por comas. 
 
-    let numero = parseInt(prompt("Ingresa un numero aquí"));
-    imprime = "";
+    // let numero = parseInt(prompt("Ingresa un numero aquí"));
+    // imprime = "";
 
-    for (let i = 0; i <= numero; i++) {
+    // for (let i = 0; i <= numero; i++) {
+    //     imprime = imprime + i;
+
+    //     if(i != numero){
+    //         imprime = imprime + ", ";
+    //     }
+    // }
+    // console.log(imprime);
+    
+}
+
+{// Ejercicio 2
+    // Se trata de que dado un número entero el script muesre una cuenta atrás,
+    // es decir, una lista de numeros separados por coma desde el número tecleado hasta el 0.
+    // Si tecle o número 5 deberá mostarar 5,4,3,2,1.
+
+    let numero = parseInt(prompt("Ingresa un número aquí"));
+    let imprime = "";
+    
+    for (let i = numero; i >= 0; i--) {
         imprime = imprime + i;
-
-        if(i != numero){
+    
+        if (i !== 0) {
             imprime = imprime + ", ";
         }
     }
     
     console.log(imprime);
-    
 }
+
+
 
 
