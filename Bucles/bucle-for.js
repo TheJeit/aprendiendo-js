6b9f03d43acdd3db0d138149c594a2aa7d6f63cf
TheJeit <soyjeit1@gmail.com>
2023-06-10 18:25:43 -0400

    Ejercicio 3 - For

--- Bucles/bucle-for.js
+++ Bucles/bucle-for.js
@@ -32,20 +32,47 @@
     // es decir, una lista de numeros separados por coma desde el número tecleado hasta el 0.
     // Si tecle o número 5 deberá mostarar 5,4,3,2,1.
 
-    let numero = parseInt(prompt("Ingresa un número aquí"));
-    let imprime = "";
+    // let numero = parseInt(prompt("Ingresa un número aquí"));
+    // let imprime = "";
     
-    for (let i = numero; i >= 0; i--) {
-        imprime = imprime + i;
+    // for (let i = numero; i >= 0; i--) {
+    //     imprime = imprime + i;
     
-        if (i !== 0) {
-            imprime = imprime + ", ";
-        }
-    }
+    //     if (i !== 0) {
+    //         imprime = imprime + ", ";
+    //     }
+    // }
     
-    console.log(imprime);
+    // console.log(imprime);
 }
 
+{//Ejercicio 3
+    // Se quiere un bucle que genere una lista con todos los números pares
+    // positivos por debajo del número tecleado por el usuario. Los números
+    // se aparecerán en una ventana alert con cambios de linea (carácter '\n').
+
+    // Si tecleo el número el número 9 deberá mostrar cinco lineas
+    // con los números 0 2 4 6 8
+
+    let numero = parseInt(prompt("Ingresa un numero"));
+    let lista = "";
+
+    for (let i = 0; i < numero; i++) {
+        if (i % 2 === 0) {
+            lista = lista + `${i}\n`;
+        }
+    }
+    alert(lista)
+}
+
+{//Ejercicio 4
+    // Este script debe escribir la tabla de multiplicar del número que el usuario teclee.
+    // La tabla mostrará en diferentes líneas cada producto y el resultado en la forma a x b = ab.
+    // Recuerda que el salto de linea es el carácter "\n"
+
+
+
+}
 
 
 
