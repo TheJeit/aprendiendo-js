597b099f52dc0140b731efc1e90ecd0f17ec70b4
TheJeit <soyjeit1@gmail.com>
2023-06-11 20:59:06 -0400

    Ejercicio 6 - FOR

--- Bucles/bucle-for.js
+++ Bucles/bucle-for.js
@@ -54,25 +54,73 @@
     // Si tecleo el número el número 9 deberá mostrar cinco lineas
     // con los números 0 2 4 6 8
 
-    let numero = parseInt(prompt("Ingresa un numero"));
-    let lista = "";
+    // let numero = parseInt(prompt("Ingresa tu número"));
+    // let lista = "";
 
-    for (let i = 0; i < numero; i++) {
-        if (i % 2 === 0) {
-            lista = lista + `${i}\n`;
-        }
-    }
-    alert(lista)
+    // for (let i = 0; i < numero; i++) {
+    //     if (i % 2 === 0) {
+    //         lista = lista + `${i}\n`;
+    //     }
+    // }
+    // alert(lista)
 }
 
 {//Ejercicio 4
-    // Este script debe escribir la tabla de multiplicar del número que el usuario teclee.
-    // La tabla mostrará en diferentes líneas cada producto y el resultado en la forma a x b = ab.
-    // Recuerda que el salto de linea es el carácter "\n"
+    // Este script debe escribir la tabla de multiplicar del número que el usuario
+    // teclee. La tabla mostrará en diferentes líneas cada producto y el resultado
+    // en la forma a x b = ab. Recuerda que el salto de linea es el carácter "\n"
 
-
-
+    // let tabla = parseInt(prompt("Ingresa tu número"));
+    // let imprime = "";
+    
+    // for (let i = 1; i <= tabla; i++) {
+    //     let producto = tabla * i;
+    //     imprime = imprime + `${tabla} x ${i} = ${producto}\n`;
+    // }
+    
+    // alert(imprime);
 }
 
+{//Ejercicio 5
+    // En este ejercicio mezclamos bucles on condicionales: el usuario escribe un número
+    // y el script imprimirá (console.log) todos los números menores que al dividirlos por
+    // 3 den como resto 2. Recuerda: un número es múltiplo de N es cuando el dividirlo por
+    // N el resto es 0.
+
+    // numero = parseInt(prompt("Ingresa tu número"));
+    // imprime = "";
+
+    // for (let i = 0; i < numero; i++) {
+    //     if (i % 3 === 2) {
+    //         imprime = imprime + `${i} `;
+    //     }
+    // }
+    // console.log(imprime);
+}
+
+{//Ejercicio 6
+    // Este script el usuario deberá teclear una letra y un número, y el programa mostrará una cadena
+    // formada por la letra repetida el número que haya tecleado.
+
+    // Si el usuario teclea x y 5 el script mostrará xxxxx. O sea una cadena de 5 letras x
+
+    let letra = "x";
+    let numero = 5;
+    let imprime = [];
+
+    for (let i = 0; i < numero; i++) {
+        imprime = `${imprime}${letra}`;
+    }
+    console.log(imprime);
+}
+
+{//Ejercicio 7
+    // Se quiere tener un contador de dos dígitos en base 5. El funcionamiento es simple:
+    // el dígito de la derecha irá aumentando y cuando pase de 4 su valor se pondrá a 0
+    // y el dígito de la izquierda se incrementa en 1. El contador debe deternerse cuando
+    // alcancemos el valor 1:4
+
+    
+}
 
 
