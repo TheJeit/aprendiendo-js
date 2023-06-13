55741855f85d59e892312a9fd4ff77a3af04db99
TheJeit <soyjeit1@gmail.com>
2023-06-12 22:19:58 -0400

    Ejercicio 9 - For

--- Bucles/bucle-for.js
+++ Bucles/bucle-for.js
@@ -24,7 +24,7 @@
     //     }
     // }
     // console.log(imprime);
-    
+
 }
 
 {// Ejercicio 2
@@ -34,15 +34,15 @@
 
     // let numero = parseInt(prompt("Ingresa un número aquí"));
     // let imprime = "";
-    
+
     // for (let i = numero; i >= 0; i--) {
     //     imprime = imprime + i;
-    
+
     //     if (i !== 0) {
     //         imprime = imprime + ", ";
     //     }
     // }
-    
+
     // console.log(imprime);
 }
 
@@ -72,12 +72,12 @@
 
     // let tabla = parseInt(prompt("Ingresa tu número"));
     // let imprime = "";
-    
+
     // for (let i = 1; i <= tabla; i++) {
     //     let producto = tabla * i;
     //     imprime = imprime + `${tabla} x ${i} = ${producto}\n`;
     // }
-    
+
     // alert(imprime);
 }
 
@@ -104,14 +104,14 @@
 
     // Si el usuario teclea x y 5 el script mostrará xxxxx. O sea una cadena de 5 letras x
 
-    let letra = "x";
-    let numero = 5;
-    let imprime = [];
+    // let letra = String(prompt("Ingresa tu letra"));
+    // let numero = parseInt(prompt("Ingresa tu número"));
+    // let imprime = "";
 
-    for (let i = 0; i < numero; i++) {
-        imprime = `${imprime}${letra}`;
-    }
-    console.log(imprime);
+    // for (let i = 0; i < numero; i++) {
+    //     imprime = `${imprime}${letra}`;
+    // }
+    // console.log(imprime);
 }
 
 {//Ejercicio 7
@@ -120,7 +120,56 @@
     // y el dígito de la izquierda se incrementa en 1. El contador debe deternerse cuando
     // alcancemos el valor 1:4
 
-    
+    // for (let i = 0; i <= 1; i++) {
+
+    //     for (let j = 0; j < 5; j++) {
+    //         // Verificar si es el último valor deseado 1:4
+    //         if (i === 1 && j === 4) {
+    //             console.log(`${i}:${j}`);
+    //             break; // Detener el bucle interno
+    //         }
+            
+    //         console.log(`${i}:${j}`);
+    //     }
+    // }
+}
+
+{//Ejercicio 8
+//     Se trata de dibujar un triángulo con asteriscos. El usuario tecleará un valor entero,
+//      el programa escribirá con asteriscos tantas lineas como diga ese número. Cada linea estará
+//       formada por una serie de astericos tan larga como diga el número de línea en el que está.
+
+//      Para separar una linea de la siguiente en console o en alert debes usar "\n".
+//      En este ejercicio usa console.log.
+
+
+    // let num = parseInt(prompt("Ingresa un numero"));
+    // let imprime = "";
+
+    // for (let i = 0; i < num; i++){
+    //     imprime = imprime + "*".repeat(i + 1) + "\n";
+    // }
+    // console.log(imprime);
+
+}
+
+{//Ejercicio 9
+    // Se trata de crear una aplicación que calcule la suma de todos los números enteros positivos
+    // entre 0 y el número tecleado por el usuario. El resultado será de la fomra
+    // "Los números enteros de 0 a n  suman suma"
+
+    // let num = parseInt(prompt("ingrese un numero"));
+    // let imprime = 0;
+
+    // for (let i = 1; i <= num; i++){
+    //     imprime = imprime + i;
+    // }
+    // console.log(imprime);
+}
+
+{//Ejercicio 10
+    // Programa un script que pida al usuario que teclee 4 números (uno cada vez). Con esos números
+    // el programa deberá calcular la media aritmética. O sea la suma de los 4 números dividida entre 4
 }
 
 
