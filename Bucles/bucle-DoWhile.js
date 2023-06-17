d0650f3dfd3b1548f351454a777321da74b655ed
TheJeit <soyjeit1@gmail.com>
2023-06-17 17:51:04 -0400

    Bucle Do While

---
+++ Bucles/bucle-DoWhile.js
@@ -0,0 +1,55 @@
+// "use strict"
+
+//Bucle Do While
+
+// let years = 30;
+
+// do{
+//     alert("HOLA")
+//     years--;
+// } while(years > 25);
+
+// Ejercicios
+
+{//Ejercicio 1
+    // Realiza un script que le pida al usuario que telee la letra A (en mayúscula) y mientras no lo haga se
+    // lo vuelva a pedir. Usa un bulce do..while y la ventana alert para los mensajes
+
+    //El usuario teclea b, el programa vuelve a pedirme que teclee una letra
+
+    // let letra;
+
+    // do {
+    //     letra = prompt("Teclea la letra A (en mayúscula)");
+    //     if (letra !== "A") {
+    //         alert("Teclea la letra A");
+    //     }
+    // } while (letra !== "A");
+}
+
+{//Ejercicios 2
+
+    // En este script vas a crear un menu con tres opciones: escribir, leer, salir. El usuario elegirá
+    // una opción y en un alert se le indicará la opción elegida. El programa vuelve a mostrar el menu y se
+    // repite el proceso. El script acabará cuando se pulse la opción de salir.
+
+    //El menu tiene 3 opciones, elijo la opción 2, respuesta "has elegido leer".
+
+    // let opcion;
+
+    // do {
+    //     opcion = parseInt(prompt("Menú:\n1. Escribir\n2. Leer\n3. Salir\nElige una opción"));
+
+    //     if (opcion === 1) {
+    //         alert("Has elegido Escribir");
+    //     } else if (opcion === 2) {
+    //         alert("Has elegido Leer");
+    //     } else if (opcion === 3) {
+    //         alert("Has elegido Salir");
+    //     } else {
+    //         alert("Opción inválida. Por favor, elige una opción válida.");
+    //     }
+    // } while (opcion !== 3);
+
+
+}
