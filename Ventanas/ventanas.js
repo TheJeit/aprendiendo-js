d9c9892c98ca3d24fccfbd79d43931bb59f65367
TheJeit <soyjeit1@gmail.com>
2023-06-17 18:21:29 -0400

    JavaScript

---
+++ Ventanas/ventanas.js
@@ -0,0 +1,14 @@
+//ALERTA
+
+//alert("Esta es mi alerta")
+
+//CONFIRMACIÓN
+
+//let resultado = confirm("Estas seguro?")
+//console.log(resultado);
+//Si digo SI me dira true y si pongo NO entonces es False
+
+//INGRESO DE DATOS
+
+let resultado2 = prompt("Edad?", 18)
+console.log(typeof parseInt(resultado2));
